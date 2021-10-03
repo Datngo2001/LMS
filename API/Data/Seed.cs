83fe3fd05de8b3c1918945fb9328159c726fdc3f
Dat Ngo <ngominhdat110115@gmail.com>
2021-10-03 19:03:59 +0700

    seed user role

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -12,17 +12,40 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager
+        , RoleManager<AppRole> roleManager)
         {
             if (await userManager.Users.AnyAsync()) return;
 
             var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
 
+            if (users == null) return;
+
+            var roles = new List<AppRole>{
+                new AppRole{Name = "Student"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Teacher"}
+            };
+
+            foreach (var role in roles)
+            {
+                await roleManager.CreateAsync(role);
+            }
+
             foreach (var user in users)
             {
                 await userManager.CreateAsync(user, "Pa$$w0rd");
+                await userManager.AddToRoleAsync(user, "Student");
             }
+
+            var admin = new AppUser
+            {
+                UserName = "admin"
+            };
+
+            await userManager.CreateAsync(admin, "Pa$$w0rd");
+            await userManager.AddToRolesAsync(admin, new[] { "Admin", "Teacher" });
         }
     }
 }
