e50e1644f72e5631e1f226824384c0c74575b4fb
Dat Ngo <ngominhdat110115@gmail.com>
2021-11-21 15:34:34 +0700

    remove use authentication

--- API/Startup.cs
+++ API/Startup.cs
@@ -60,7 +60,6 @@ namespace API
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
             
-            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseDefaultFiles();
